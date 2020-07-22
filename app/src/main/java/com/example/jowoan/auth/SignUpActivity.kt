43a4c61f1b317dd9625f2a8f9e24b3305284bb5e
idmdragon <ilham.uklison@gmail.com>
2020-07-22 11:18:52 +0700

    layout sign up

--- app/src/main/java/com/example/jowoan/auth/SignUpActivity.kt
+++ app/src/main/java/com/example/jowoan/auth/SignUpActivity.kt
@@ -1,12 +1,32 @@
 package com.example.jowoan.auth
 
+import android.content.Intent
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.jowoan.MainActivity
 import com.example.jowoan.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        tvMasuk.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+
+
+        tvMasuk.setOnClickListener {
+            Intent(applicationContext, LoginActivity::class.java).also {
+                startActivity(it)
+
+            }
+        }
+
+        btnBuatAkun.setOnClickListener {
+            Intent(applicationContext, MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
